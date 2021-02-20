360470984d6278ad7ea2c6b7377aef3c56c31fca
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-20 20:32:30 +0700

    Tugas 13 JS

---
+++ Tugas 13 JS.js
@@ -0,0 +1,10 @@
+function buahbuahan() {
+    var buah = ['Pisang', 'Jeruk'];
+    console.log(buah);
+    buah.unshift('Apel', 'Mangga');
+    //Jika menggunakan splice
+    // buah.splice(0,0,'Apel','Durian');
+    return buah;
+}
+
+console.log(buahbuahan());
